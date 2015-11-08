3f70e25e8fce37172228a40b647c756a290e55a6
unknown <jlangowitz@HZYPFV1.olin.edu>
2015-11-08 11:46:26 -0500

    server stuff

---
+++ Server/app.py
@@ -0,0 +1,28 @@
+"""
+HFID F2015
+Team 2Hot2Cold
+Emily Guthrie, Josh Langowitz, Ankeet Mutha, Brooks Willis
+Website
+"""
+# imports
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash, json, jsonify
+import os
+from werkzeug import secure_filename
+
+# config
+DEBUG = True
+SECRET_KEY = 'i6TIU_oI7yoiHp_p9Ppu_oGTGiu6Fdy5ufr__sda-3'
+USERNAME = 'toomanybricksonthedancefloor'
+PASSWORD = 'default'
+
+# app setup
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('index.html', title='Task.io')
+
+if __name__ == '__main__':
+    app.run()
